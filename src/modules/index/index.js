3e7d57bed506efd5b81dcaea6c0e45217fc9b58e
mrhop <233402923@qq.com>
2017-12-22 13:57:27 +0800

    Initial commit

---
+++ src/modules/index/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import chosen from ''
+/* eslint-disable */
+// https://reactjs.org/docs/error-boundaries.html
+class Chosen extends React.Component {
+  componentDidMount() {
+    this.$el = $(this.el);
+    this.$el.chosen();
+  }
+
+  componentWillUnmount() {
+    this.$el.chosen('destroy');
+  }
+
+  render() {
+    return (
+      <div>
+        <select className="Chosen-select" ref={el => this.el = el}>
+          {this.props.children}
+        </select>
+      </div>
+    );
+  }
+}
+
+function Example() {
+  return (
+    <Chosen>
+      <option>vanilla</option>
+      <option>chocolate</option>
+      <option>strawberry</option>
+    </Chosen>
+  );
+}
+
+ReactDOM.render(
+  <Example />,
+  document.getElementById('root')
+);
